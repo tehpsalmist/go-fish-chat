097748cf94554283e893013b324d51e64a467b26
tehpsalmist <bstewardcodes@gmail.com>
2022-01-26 10:00:23 -0500

    ramping up

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+export const App = props => {
+  return <main className='bg-indigo min-h-screen flex-center text-green-400'>Hello World!</main>
+}
+
+render(<App />, document.getElementById('app'))
