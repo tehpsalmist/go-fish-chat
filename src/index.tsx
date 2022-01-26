0a88522212b640189df7618798099e683424b1b2
tehpsalmist <bstewardcodes@gmail.com>
2022-01-26 10:12:20 -0500

    google

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 
 export const App = props => {
-  return <main className='bg-indigo min-h-screen flex-center text-green-400'>Hello World!</main>
+  return <main className='bg-indigo-600 min-h-screen flex-center text-green-400'>Hello World!</main>
 }
 
 render(<App />, document.getElementById('app'))
